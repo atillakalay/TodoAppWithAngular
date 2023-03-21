a795b4a4e3bbe08a004647e5f1a5053006d23ed1
atillakalay <atilla.kalayy@gmail.com>
2023-03-21 20:11:41 +0300

    ToDo App Part 10 lesson completed.

--- TodoApp/src/app/pages/todo-update/todo-update.component.ts
+++ TodoApp/src/app/pages/todo-update/todo-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TodoService } from 'src/app/services/TodoService';
 
 @Component({
   selector: 'app-todo-update',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-update.component.css']
 })
 export class TodoUpdateComponent {
+
+  todoForm = new FormGroup({
+    content: new FormControl(''),
+    id: new FormControl()
+  })
+  id: number | undefined
+  constructor(private todoService: TodoService, private router: Router, private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'))
+    this.todoService.get(this.id).subscribe(x => {
+      this.todoForm.get('content')?.setValue(x.content)
+      this.todoForm.get('id')?.setValue(x.id)
+    })
+
+  }
+
+  update(){
+    
+  }
+
+
+
+
 
 }
