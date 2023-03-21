ce96cf17ef9f14ccd2e032e14323c55ebf96b1ab
atillakalay <atilla.kalayy@gmail.com>
2023-03-21 15:10:58 +0300

    ToDo App Part 4 lesson is completed.

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoAddComponent } from './pages/todo-add/todo-add.component';
 import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { environment } from 'environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoAddComponent,
     TodoUpdateComponent,
-    TodoListComponent
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{provide:'BASE_API_URL',useValue:environment.baseUrl}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
